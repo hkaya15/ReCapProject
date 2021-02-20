6b1bf7c4550bd71cfc549e69cafb2a90fea3d211
Hüseyin Kaya <kayahuseyin15@gmail.com>
2021-02-21 01:13:45 +0300

    Added AOP, Added ValidationAspect

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results.Abstract;
 using Core.Utilities.Results.Concrete;
@@ -22,11 +23,12 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
+
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            ValidationTool.Validate(new CarValidator(), car);
-                _carDal.Add(car);
-                return new SuccessResult(Messages.CarAdded);
+            _carDal.Add(car);
+            return new SuccessResult(Messages.CarAdded);
         }
 
         public IResult Delete(Car car)
@@ -37,26 +39,26 @@ namespace Business.Concrete
 
         public IDataResult<List<Car>> GetAll()
         {
-           /* if(DateTime.Now.Hour>=17 || DateTime.Now.Hour <= 08)
-            {
-                return new ErrorDataResult<List<Car>>(Messages.OutUse);
-            }*/
-            return new SuccessDataResult<List<Car>>(_carDal.GetAll(),Messages.CarListed);
+            /* if(DateTime.Now.Hour>=17 || DateTime.Now.Hour <= 08)
+             {
+                 return new ErrorDataResult<List<Car>>(Messages.OutUse);
+             }*/
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(), Messages.CarListed);
         }
 
         public IDataResult<List<CarDetailDto>> GetCarDetails()
         {
-          /*  if (DateTime.Now.Hour >= 17 || DateTime.Now.Hour<=08)
-            {
-                return new ErrorDataResult<List<CarDetailDto>>(Messages.OutUse);
-            }*/
-            return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails(),Messages.CarDetailed);
+            /*  if (DateTime.Now.Hour >= 17 || DateTime.Now.Hour<=08)
+              {
+                  return new ErrorDataResult<List<CarDetailDto>>(Messages.OutUse);
+              }*/
+            return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails(), Messages.CarDetailed);
         }
 
         public IDataResult<List<Car>> GetCarsByBrandId(int id)
         {
-           return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.BrandId == id));
-           
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.BrandId == id));
+
         }
 
         public IDataResult<List<Car>> GetCarsByColorId(int id)
---
+++ Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Castle.DynamicProxy;
+using System;
+
+namespace Core.Utilities.Interceptors
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
+    {
+        public int Priority { get; set; }
+        //IInvocation çağrılan metot ile ilgili tüm bilgileri içerisinde barındırır
+        public virtual void Intercept(IInvocation invocation)
+        {
+           
+        }
+    }
+}
